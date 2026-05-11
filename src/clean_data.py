c5da3fe8673e80302569401d39cb7b02977d756f
Mateusz <mateuszhorabik31@gmail.com>
2026-05-11 12:01:18 +0200

    Check data quality and clean columns

---
+++ src/clean_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from pathlib import Path
+
+
+RAW_DATA_PATH = Path("data/raw/SampleSuperstore.csv")
+CLEANED_DATA_PATH = Path("data/cleaned/cleaned_data.csv")
+df = pd.read_csv(RAW_DATA_PATH)
+
+def clean_column_name(df):
+    df.columns = (
+        df.columns
+        .str.strip()
+        .str.lower()
+        .str.replace(" ", "_")
+        .str.replace("-", "_")
+    )
+    return df
+
+print(f"\n{df.duplicated().sum()}")
