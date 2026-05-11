cf77a5f7e467cc883853bfb1cc15c42c7f9e20bc
Mateusz <mateuszhorabik31@gmail.com>
2026-05-11 12:15:43 +0200

    Add clean data to new file

--- src/clean_data.py
+++ src/clean_data.py
@@ -3,9 +3,8 @@ from pathlib import Path
 
 
 RAW_DATA_PATH = Path("data/raw/SampleSuperstore.csv")
-CLEANED_DATA_PATH = Path("data/cleaned/cleaned_data.csv")
-df = pd.read_csv(RAW_DATA_PATH)
-df["Order Date"] = pd.to_datetime(df["Order Date"])
+PROCCESSED_DATA_PATH = Path("data/cleaned/cleaned_data.csv")
+
 
 def clean_column_name(df):
     df.columns = (
@@ -17,4 +16,15 @@ def clean_column_name(df):
     )
     return df
 
-print(f"\n{df.duplicated().sum()}")
+df = pd.read_csv(RAW_DATA_PATH)
+df["Order Date"] = pd.to_datetime(df["Order Date"])
+df["Ship Date"] = pd.to_datetime(df["Ship Date"])
+df = clean_column_name(df)
+df = df.drop_duplicates(df)
+
+important_columns = ["quantity", "sales", "profit"]
+existing_columns = [col for col in important_columns if col in df.columns]
+df.dropna(subset=["sales", "profit", "quantity"])
+
+PROCCESSED_DATA_PATH.parent.mkdir(parents=True, exist_ok=True)
+df.to_csv(PROCCESSED_DATA_PATH, index=False)
