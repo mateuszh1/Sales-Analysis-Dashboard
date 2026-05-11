cc8ddd3ea60f77e5d8dc8203fd5fa4ac73d3f767
Mateusz <mateuszhorabik31@gmail.com>
2026-05-11 12:22:52 +0200

    Cleand csv file

--- src/clean_data.py
+++ src/clean_data.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 RAW_DATA_PATH = Path("data/raw/SampleSuperstore.csv")
-PROCCESSED_DATA_PATH = Path("data/cleaned/cleaned_data.csv")
+PROCCESSED_DATA_PATH = Path("data/PROCCESSED/cleaned_data.csv")
 
 
 def clean_column_name(df):
@@ -17,14 +17,19 @@ def clean_column_name(df):
     return df
 
 df = pd.read_csv(RAW_DATA_PATH)
-df["Order Date"] = pd.to_datetime(df["Order Date"])
-df["Ship Date"] = pd.to_datetime(df["Ship Date"])
+
+
 df = clean_column_name(df)
-df = df.drop_duplicates(df)
+
+df = df.drop_duplicates()
 
 important_columns = ["quantity", "sales", "profit"]
 existing_columns = [col for col in important_columns if col in df.columns]
-df.dropna(subset=["sales", "profit", "quantity"])
+df = df.dropna(subset=["sales", "profit", "quantity"])
 
 PROCCESSED_DATA_PATH.parent.mkdir(parents=True, exist_ok=True)
 df.to_csv(PROCCESSED_DATA_PATH, index=False)
+
+print("File load successfully")
+print(f"{len(df)}")
+print(f"Clean data saved to {PROCCESSED_DATA_PATH}")
